73ded925832e627c8d76e9ef2a31f8985d4521ef
Minjune <minjune8506@gmail.com>
2026-03-01 14:38:38 +0900

    Moldoo React

---
+++ src/data/videos.js
@@ -0,0 +1,52 @@
+export const videos = [
+  {
+    src: 'https://www.youtube.com/embed/XrIR6-hb7X8?si=4lLE--VavXxxBpvH',
+    title: '다시, 푸들 | 반려동물구조협회',
+    caption: '다시, 푸들 | 반려동물구조협회',
+  },
+  {
+    src: 'https://www.youtube.com/embed/pC7Xsi3c9UQ?si=kjDjubc53VsaRZoU',
+    title: 'Rise on One | AI 뮤직비디오',
+    caption: 'Rise on One | AI 뮤직비디오',
+  },
+  {
+    src: 'https://www.youtube.com/embed/d5BJ5o_gF5Y?si=lYx-sCPMwRzXgKCh',
+    title: '교육용 영상 | K-edu(PLANTGROW)',
+    caption: '교육용 영상 | K-edu(PLANTGROW)',
+  },
+  {
+    src: 'https://www.youtube.com/embed/lrlWkzcqF1s?si=24GaWSeff3PmmTOt',
+    title: '포트폴리오 영상 | MC진심',
+    caption: '포트폴리오 영상 | MC진심',
+  },
+  {
+    src: 'https://www.youtube.com/embed/PMW_5Ycpx1I?si=UBvNDkj_YIyJYNNG',
+    title: '덴마크 정자은행 | 메디칼티카',
+    caption: '덴마크 정자은행 | 메디칼티카',
+  },
+  {
+    src: 'https://www.youtube.com/embed/8ewkAcvMPJ8?si=QYW98Ikg0-G5u638',
+    title: '로맨스 코드 | 전북도민일보',
+    caption: '로맨스 코드 | 전북도민일보',
+  },
+  {
+    src: 'https://www.youtube.com/embed/XrEeRc6gSkk?si=TA9EBpoHC2lAt2bc',
+    title: '브랜드 영상 | 비아로',
+    caption: '브랜드 영상 | 비아로',
+  },
+  {
+    src: 'https://www.youtube.com/embed/agN0QS8i1uk?si=PqZgCt16I--fNJtf',
+    title: '브이로그 영상 | 개인',
+    caption: '브이로그 영상 | 개인',
+  },
+  {
+    src: 'https://www.youtube.com/embed/Z6FHmi6E1ck?si=5POXUmWt61b6i2tl',
+    title: '기업 영상 | 푸른파트너스',
+    caption: '기업 영상 | 푸른파트너스',
+  },
+  {
+    src: 'https://www.youtube.com/embed/TetWejyUpv0?si=sqAf7kAXR2_lUyk3',
+    title: '화성공모전',
+    caption: '화성공모전',
+  },
+];
